38824e1fe1b2a0b75885c3fe32e626ce2b196832
Ilka Djajakusuma <e0325512@student.tuwien.ac.at>
2016-09-15 21:04:49 +0200

    angular material

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,6 +1,4 @@
 //console.log('This would be the main JS file.');
 
-function onClick() {
-    alert("Hello!");
-    alert("How are you?");
-}
+angular
+    .module('firstApplication', ['ngMaterial']);
